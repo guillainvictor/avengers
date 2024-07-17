b87c67a0bb132ae39138d3ec9b0a6ec4d6b5fa21
guillainvictor <raziel36@hotmail.fr>
2024-07-17 22:39:22 +0200

    fix fault

--- article-avengers.php
+++ article-avengers.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
     <head>
-        <title><?= "Mettre variable _GET" ?></title>
+        <title>Les avengers</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Guillain Victor">
-        <meta name="description" content="Présentation des avengers avec photo des principaux menbres.">
+        <meta name="description" content="Page de présentation des Avengers avec les photos des principaux menbres de la saga de l'infini.">
         <link rel="stylesheet" href="./css/style-avengers.css">
-        <link rel="icon" href="../images/avengers/favicon.ico" type="image/x-con">
+        <link rel="icon" href="./images/avengers/favicon.ico" type="image/x-con">
         <link href="https://fonts.googleapis.com/css2?family=Rethink+Sans:wght@500&display=swap" rel="stylesheet">
     </head>
 
     <body>
-        <div aria-label="page content" class="wrapper">
+        <div class="wrapper">
             <header>
-                <h1>Les <img src='./images/avengers/logo.webp' alt="logo avengers"> <span aria-label="container fin du h1">: héros et protecteurs!</span></h1>
+                <h1>Les <img src='./images/avengers/logo.webp' alt="logo des avengers"> <span>: héros et protecteurs!</span></h1>
 
                 <nav>
-                    <section aria-label="link website">
-                        <a href="#">A propos</a>
-                        <a href="#">Blog</a>
-                        <a href="#">Contact</a>
+                    <section>
+                        <a href="#" aria-label="accéder à cette page">A propos</a>
+                        <a href="#" aria-label="accéder à cette page">Blog</a>
+                        <a href="#" aria-label="accéder à cette page">Contact</a>
                     </section>
 
-                    <section aria-label="link social networks">
-                        <figure aria-label="container logo facebook">
+                    <section>
+                        <figure>
                             <img src="./images/avengers/facebook_icon.webp" alt="logo facebook">
                         </figure>
-                        <figure aria-label="container logo twiter">
+                        <figure>
                             <img src="./images/avengers/twitter_icon.webp" alt="logo twiter">
                         </figure>
                     </section>
@@ -36,69 +36,73 @@
             </header>
 
             <main>
-                <section aira-label="presentation of avengers">
-                    <h2>Les <span aria-label="avengers en red">Avengers</span>, les meilleurs amis des citoyens du monde</h2>
-                    <p>Les Avengers sont une équipe de super-héros exceptionnels qui travaillent ensemble pour protéger le mond des menaces intérieures et extérieures. 
+                <section>
+                    <h2>Les <span>Avengers</span>, les meilleurs amis des citoyens du monde</h2>
+                    <p>Les Avengers sont une équipe de super-héros exceptionnels qui travaillent ensemble pour protéger le monde des menaces intérieures et extérieures. 
                         Ils sont incroyablement puissants, mais ils ont également un sens de l'humour et une humanité qui les rendent attachants.
                         Lorsque les Avengers sont en action, vous savez que tout va bien se passer!
                     </p>
                 </section>
 
-                <section aria-label="container picture of Iron man">
-                    <figure aria-label="picture of Iron man">
-                        <img src="./images/avengers/iron-man.webp" alt="photo d'Iron Man">
+                <section>
+                    <figure>
+                        <img src="./images/avengers/iron-man.webp" alt="Iron-man en armure qui a genou à terre">
                         <figcaption>Le super-héros de la semaine <i></i></figcaption>
                     </figure>
                 </section>
 
-                <div aria-label="container area aside and hero's pictures">
-                    <section aria-labelledby="container of hero's pictures">
-                        <div aria-labelledby="container first five pictures">
-                            <div aria-label="container first pictures" class="firstPictures">
-                                <figure aria-label="container héro"><img src="./images/avengers/captain-america.webp" alt="photo de captain america"></figure>
+                <div>
+                    <section>
+                        <div>
+                            <div class="firstPictures">
+                                <figure>
+                                    <img src="./images/avengers/captain-america.webp" alt="captain america levant son bouclier">
+                                </figure>
                             </div>
-                            <div aria-label="container four pictures following" class="secondFourPicture img">
-                                <figure aria-label="container héro"><img src="./images/avengers/thor.webp" alt="photo de thor"></figure>
-                                <figure aria-label="container héro"><img src="./images/avengers/hulk.webp" alt="photo de hulk"></figure>
-                                <figure aria-label="container héro" class="first-picture-widow"><img src="./images/avengers/black-widow.webp" alt="photo de black widow"></figure>
-                                <figure aria-label="container héro" class="first-picure-hawkeye"><img src="./images/avengers/hawkeye.webp" alt="photo de hawkeye"></figure>
+
+                            <div class="secondFourPicture img">
+                                <figure><img src="./images/avengers/thor.webp" alt="thor levant son marteau et envoie des éclairs"></figure>
+                                <figure><img src="./images/avengers/hulk.webp" alt="hulk qui sort des décombres entouré de fumée"></figure>
+                                <figure class="first-picture-widow"><img src="./images/avengers/black-widow.webp" alt="black widow tenant un pistolet devant une scène de bataille"></figure>
+                                <figure class="first-picure-hawkeye"><img src="./images/avengers/hawkeye.webp" alt="hawkeye qui s'apprête à tirer à l'arc"></figure>
                             </div>
                         </div>
-                        <div aria-label="container pictures following" class="lastPictures img">
-                            <figure aria-label="container héro" class="second-picure-captain-america"><img src="./images/avengers/captain-america.webp" alt="photo de captain america"></figure>
-                            <figure aria-label="container héro" class="second-picure-thor"><img src="./images/avengers/thor.webp" alt="photo de thor"></figure>
-                            <figure aria-label="container héro" class="second-picure-hulk"><img src="./images/avengers/hulk.webp" alt="photo de hulk"></figure>  
-                            <figure aria-label="container héro" class="second-picture-widow"><img src="./images/avengers/black-widow.webp" alt="photo de black widow"></figure>
-                            <figure aria-label="container héro" class="second-picure-hawkeye"><img src="./images/avengers/hawkeye.webp" alt="photo de hawkeye"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/scarlet-witch.webp" alt="photo de scarlet-witch"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/vision.webp" alt="photo de vision"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/antman.webp" alt="photo de antman"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/falcon.webp" alt="photo de falcon"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/war-machine.webp" alt="photo de war-machine"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/spiderman.webp" alt="photo de spiderman"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/doctor-strange.webp" alt="photo de doctor-strange"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/black-panther.webp" alt="photo de black-panther"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/captain-marvel.webp" alt="photo de captain-marvel"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/winter-soldier.webp" alt="photo de winter-soldier"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/star-lord.webp" alt="photo de star-lord"></figure>
-                            <figure aria-label="container héro"><img src="./images/avengers/nick-fury.webp" alt="photo de nick-fury"></figure>
+
+                        <div class="lastPictures img">
+                            <figure class="second-picure-captain-america"><img src="./images/avengers/captain-america.webp" alt="captain america levant son bouclier"></figure>
+                            <figure class="second-picure-thor"><img src="./images/avengers/thor.webp" alt="thor levant son marteau et envoie des éclairs"></figure>
+                            <figure class="second-picure-hulk"><img src="./images/avengers/hulk.webp" alt="hulk qui sort des décombres entouré de fumée"></figure>  
+                            <figure class="second-picture-widow"><img src="./images/avengers/black-widow.webp" alt="black widow tenant un pistolet devant une scène de bataille"></figure>
+                            <figure class="second-picure-hawkeye"><img src="./images/avengers/hawkeye.webp" alt="hawkeye qui s'apprête à tirer à l'arc"></figure>
+                            <figure><img src="./images/avengers/scarlet-witch.webp" alt="scarlet-witch debout dans une tenu rouge"></figure>
+                            <figure><img src="./images/avengers/vision.webp" alt="vision avec la main levé dos à des armures d'iron man"></figure>
+                            <figure><img src="./images/avengers/antman.webp" alt="antman dans son costume"></figure>
+                            <figure><img src="./images/avengers/falcon.webp" alt="falcon dans son costume avec des avions de chasse à l'arrière"></figure>
+                            <figure><img src="./images/avengers/war-machine.webp" alt="war-machine debout devant un batiment"></figure>
+                            <figure><img src="./images/avengers/spiderman.webp" alt="spiderman debout dans son costume"></figure>
+                            <figure><img src="./images/avengers/doctor-strange.webp" alt="doctor-strange utilisant ses pouvoirs"></figure>
+                            <figure><img src="./images/avengers/black-panther.webp" alt="black-panther en train de courir"></figure>
+                            <figure><img src="./images/avengers/captain-marvel.webp" alt="captain-marvel debout dans son costume"></figure>
+                            <figure><img src="./images/avengers/winter-soldier.webp" alt="le soldat de l'hiver qui regarde sur sa gauche"></figure>
+                            <figure><img src="./images/avengers/star-lord.webp" alt="star-lord portant un masque et en train de tirer"></figure>
+                            <figure><img src="./images/avengers/nick-fury.webp" alt="nick-fury arme à la main devant un batiment en ruine"></figure>
                         </div>
                     </section>
 
                     <aside>
-                        <div aria-label="section avengers rassemblement">
+                        <section>
                             <h3>Avengers Rassemblement</h3>
                             <p>Vous avez des compétences hors du commun et vous souhaitez sauver le monde aux côtés des plus
                                 grands héros de la planète ?
                             </p>
-                            <a href="#">Soumettez votre candidature !</a>
-                        </div>
+                            <a href="#" aria-label="lien pour la soumission">Soumettez votre candidature !</a>
+                        </section>
 
-                        <div aria-label="section avengers rassemblement">
+                        <section>
                             <h3>Restez informé des dernières aventures des Avengers</h3>
                             <p>Abonnez-vous à notre newsletter et ne manquez aucune mission !</p>
-                            <a href="#">S'abonner</a>
-                        </div>
+                            <a href="#" aria-label="lien pour s'abonner">S'abonner</a>
+                        </section>
                     </aside>
                 </div>
             </main>
